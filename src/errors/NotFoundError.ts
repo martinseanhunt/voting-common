4ddf11c1a88ace9faaed9b7bf5a6c8c871569217
Martin Hunt <martinseanhunt@gmail.com>
2021-01-04 07:16:33 +0000

    use super

--- src/errors/NotFoundError.ts
+++ src/errors/NotFoundError.ts
@@ -4,6 +4,10 @@ export class NotFoundError extends CustomError {
   statusCode = 404
   message = '404: Route not found'
 
+  constructor() {
+    super()
+  }
+
   serializeErrors() {
     return [{ message: this.message }]
   }
