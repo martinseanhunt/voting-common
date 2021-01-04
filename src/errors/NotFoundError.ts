bb1d4868789d1f7d91a02db36b88a48957f44412
Martin Hunt <martinseanhunt@gmail.com>
2021-01-04 07:11:20 +0000

    testing whether constructor is needed at all in custom errors

--- src/errors/NotFoundError.ts
+++ src/errors/NotFoundError.ts
@@ -4,10 +4,6 @@ export class NotFoundError extends CustomError {
   statusCode = 404
   message = '404: Route not found'
 
-  constructor() {
-    super()
-  }
-
   serializeErrors() {
     return [{ message: this.message }]
   }
