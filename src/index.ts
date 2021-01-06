1b23f24681f4e003f5547bdf366017d4fedced55
Martin Hunt <martinseanhunt@gmail.com>
2021-01-06 15:55:24 +0000

    protectedroute middleware

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './middlewares/healthCheckHandler'
 export * from './middlewares/errorHandler'
 export * from './middlewares/handleValidationErrors'
 export * from './middlewares/currentUser'
+export * from './middlewares/protectedRoute'
 
 export * from './errors/BadRequestError'
 export * from './errors/NotFoundError'
