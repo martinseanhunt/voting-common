3905f27100d932854b239cbac88c814dca024383
Martin Hunt <martinseanhunt@gmail.com>
2021-01-13 08:02:36 +0000

    nats wrapper

--- src/middlewares/adminRoute.ts
+++ src/middlewares/adminRoute.ts
@@ -7,13 +7,14 @@ export const adminRoute = (req: Request, res: Response, next: NextFunction) => {
   // if there's no current user on the request then the user isn't signed in
   if (!req.currentUser) throw new AuthorizationError()
 
+  // if the user isn't an admin throw an authentication error
   if (req.currentUser.role !== Role.Admin) throw new AuthorizationError()
 
-  // Otherwise the user is signed in and we can safely call next to allow them access
+  // Otherwise the user is signed in and an admin and we can safely call next to allow them access
   // to the requested route.
-  // NOTE: We could optionally check the userDb here to make sure that the user is still valid
-  // and hasn't been deleted from the db or dissallowed access in some way. That would create a
-  // dependency on the users db for all od our services that have protected routes and I'd rather
-  // avoid that for such a simple application.
+
+  // NOTE: We could optionally check the userDb here to make sure that the user is still and
+  // admin rather than just trusting the JWT. This would create a dependancy on the user DB or
+  // service though... For now keeping things seperate
   next()
 }
