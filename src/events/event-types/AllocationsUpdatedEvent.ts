fa670f965ae6c5891df483c109c232d2bd3e2553
Martin Hunt <martinseanhunt@gmail.com>
2021-01-23 12:02:35 +0000

    add cause version to allcoations updated event

--- src/events/event-types/AllocationsUpdatedEvent.ts
+++ src/events/event-types/AllocationsUpdatedEvent.ts
@@ -4,6 +4,7 @@ export interface AllocationsUpdatedEvent {
   subject: Subjects.AllocationsUpdated
   data: {
     causeId: string
+    causeVersion: number
     totalPoints: number
     allocationsToCause: number
   }[]
